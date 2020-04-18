c0ff512fe2881ddc0424862dc1a9d2ff864f8831
Alkimus <42779677+MConlisk@users.noreply.github.com>
2020-04-18 00:16:36 -0700

    Update AssemblyInfo.cs

--- PineLog/Properties/AssemblyInfo.cs
+++ PineLog/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PineLog")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Log system that writes to a file.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Alkimus Games")]
 [assembly: AssemblyProduct("PineLog")]
